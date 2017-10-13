0d368d6336717e328b1d721d3564676b69ec1866
韩超 <yan_che@qq.com>
2017-10-13 17:42:21 +0800

    赏金任务dev

--- course-web-server/src/main/java/com/huatu/tiku/course/netschool/api/v3/UserServiceV3.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/netschool/api/v3/UserServiceV3.java
@@ -1,6 +1,7 @@
 package com.huatu.tiku.course.netschool.api.v3;
 
 import com.huatu.tiku.course.bean.NetSchoolResponse;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author hanchao
  * @date 2017/10/12 16:49
  */
+@FeignClient("course-service")
 public interface UserServiceV3 {
     /**
      * 修改用户昵称
--- course-web-server/src/main/java/com/huatu/tiku/course/spring/conf/base/RabbitMqConfig.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/spring/conf/base/RabbitMqConfig.java
@@ -2,14 +2,18 @@ package com.huatu.tiku.course.spring.conf.base;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.huatu.tiku.course.mq.listeners.RewardActionListener;
+import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import static com.huatu.tiku.course.common.RabbitQueueConsts.QUEUE_SEND_FREE_COURSE;
-import static com.huatu.tiku.course.common.RabbitQueueConsts.QUEUE_USER_NICK_UPDATE;
+import static com.huatu.tiku.course.common.RabbitQueueConsts.*;
 
 /**
  * @author hanchao
@@ -27,8 +31,8 @@ public class RabbitMqConfig {
      * queue声明
      * @return
      */
-    @Bean("send_free_course_queue")
-    public Queue queue(){
+    @Bean
+    public Queue sendFreeCourseQueue(){
         return new Queue(QUEUE_SEND_FREE_COURSE);
     }
 
@@ -36,4 +40,27 @@ public class RabbitMqConfig {
     public Queue userNickUpdateQueue(){
         return new Queue(QUEUE_USER_NICK_UPDATE);
     }
+
+
+    @Bean
+    public Queue rewardActionQueue(){
+        return new Queue(QUEUE_REWARD_ACTION);
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer manualRabbitContainer(@Autowired ConnectionFactory connectionFactory,
+                                                                @Autowired RewardActionListener rewardActionUpdateListener,
+                                                                @Autowired(required = false) ThreadPoolTaskExecutor threadPoolTaskExecutor){
+        SimpleMessageListenerContainer manualRabbitContainer = new SimpleMessageListenerContainer();
+        manualRabbitContainer.setConnectionFactory(connectionFactory);
+        if(threadPoolTaskExecutor != null){
+            manualRabbitContainer.setTaskExecutor(threadPoolTaskExecutor);
+        }
+        manualRabbitContainer.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        manualRabbitContainer.setConcurrentConsumers(threadPoolTaskExecutor.getCorePoolSize()/2);
+        manualRabbitContainer.setMaxConcurrentConsumers(threadPoolTaskExecutor.getCorePoolSize()/2);
+        manualRabbitContainer.setMessageListener(rewardActionUpdateListener);
+        return manualRabbitContainer;
+    }
+
 }
