35a5ee46fc12c5166c04f7db8c8df5c3b9b953ae
韩超 <yan_che@qq.com>
2017-10-23 09:39:36 +0800

    修改扫描包

--- course-web-server/src/main/java/com/huatu/tiku/course/CourseAppliaction.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/CourseAppliaction.java
@@ -5,16 +5,13 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.stereotype.Controller;
 
 /**
  * @author hanchao
  * @date 2017/8/18 15:43
  */
 
-@ComponentScans({@ComponentScan(value = "com.huatu.tiku.course",excludeFilters = @ComponentScan.Filter(Controller.class)),
-                 @ComponentScan(value = "com.huatu.tiku.course.web.controller.v3",includeFilters = @ComponentScan.Filter(Controller.class))})
+@ComponentScan(basePackageClasses = CourseAppliaction.class)
 @EnableAutoConfiguration
 @EnableFeignClients  // feign如果要做额外的配置，不能处于该包下，不能被component扫描到
 @SpringBootConfiguration
