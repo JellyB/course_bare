20191b02c6aba56ae55b7b96af8ed636bbbf44e3
韩超 <yan_che@qq.com>
2018-01-08 10:57:15 +0800

    降级过程中的课程列表补偿

--- course-web-server/src/main/java/com/huatu/tiku/course/service/CourseListService.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/service/CourseListService.java
@@ -12,10 +12,13 @@ import com.huatu.tiku.course.util.ResponseUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -35,6 +38,11 @@ public class CourseListService {
     private CourseServiceV3Fallback courseServiceV3Fallback;
     @Autowired
     private PromoteBizService promoteBizService;
+
+    /**
+     * 用来将降级过程中的请求缓存在此处，然后10s主动重建一次,WeakKeyConcurrentHashMap
+     */
+    private Map<String,Map> requestQueue = new ConcurrentHashMap<>();
 
     /**
      * 异步获取课程列表 v3
@@ -78,9 +86,14 @@ public class CourseListService {
     public CourseListV3DTO getCourseListV3Degrade(Map<String, Object> params) {
         params.remove("username");
 
+        //begin 为了补偿
+        String key = "_mock_live_list$" + com.huatu.common.utils.web.RequestUtil.getParamSign(params);
+        requestQueue.put(key,params);
+        // end
+
+
         NetSchoolResponse liveList = courseServiceV3Fallback.findLiveList(params);
         if (liveList.getCode() == NetSchoolResponse.DEFAULT_ERROR.getCode()) {
-            String key = "_mock_live_list$" + com.huatu.common.utils.web.RequestUtil.getParamSign(params);
             if (ConcurrentBizLock.tryLock(key)) {
                 try {
                     liveList = courseServiceV3.findLiveList(params);
@@ -98,4 +111,32 @@ public class CourseListService {
         }
         return result;
     }
+
+    /**
+     * 降级过程中的课程列表补偿
+     */
+    @Scheduled(cron = "0/10 * * * * ?")
+    public void courseListDegradeCompensate(){
+        Set<String> keys = requestQueue.keySet();
+        for (String key : keys) {
+            try {
+                Map params = requestQueue.get(key);
+                NetSchoolResponse response = courseServiceV3.findLiveList(params);
+                CourseListV3DTO result = ResponseUtil.build(response, CourseListV3DTO.class, false);
+                //非fallback获取到，设置到fallback缓存
+                if (!result.isCache()) {
+                    courseServiceV3Fallback.setLiveList(params, response);
+                }
+                params = null;
+            } catch(Exception e){
+                log.error("定时补偿课程列表缓存失败");
+            } finally {
+                requestQueue.remove(key);
+            }
+        }
+        keys = null;
+        //防止任务堆积出现问题
+        requestQueue.clear();
+    }
+
 }
