eab49f21f331101bc2529fda51343ecd0077f25d
韩超 <yan_che@qq.com>
2018-01-18 16:21:31 +0800

    dev

--- course-web-server/src/main/java/com/huatu/tiku/course/web/controller/ForTestController.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/web/controller/ForTestController.java
@@ -1,5 +1,11 @@
 package com.huatu.tiku.course.web.controller;
 
+import com.huatu.common.utils.collection.HashMapBuilder;
+import com.huatu.springboot.report.annotation.WebReport;
+import com.huatu.springboot.report.product.ExtraDataHandler;
+import com.huatu.tiku.common.bean.report.ReportMessage;
+import com.huatu.tiku.common.bean.report.WebReportMessage;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,4 +21,26 @@ public class ForTestController {
     public void exceptionTest(){
         throw new NullPointerException("抛出了一个空指针！");
     }
+
+    @WebReport(value = "test",extraHandler = TestReportExtraHandler.class)
+    @RequestMapping("/report")
+    public Object report(){
+        return HashMapBuilder.newBuilder().put("llll","llsdfdfd").buildUnsafe();
+    }
+
+
+    @Component
+    public static class TestReportExtraHandler implements ExtraDataHandler {
+
+        @Override
+        public Object extra(ReportMessage reportMessage) {
+            if(reportMessage instanceof WebReportMessage){
+                String body = ((WebReportMessage) reportMessage).getBody();
+                //比如用户请求的是json或者什么的，我们需要保存某些信息，或者计算某些信息(无法从response直接取得数据去判断，而且和controller异步执行，所以可能需要重复逻辑)
+                return body.length();
+            }
+            return null;
+        }
+    }
+
 }
