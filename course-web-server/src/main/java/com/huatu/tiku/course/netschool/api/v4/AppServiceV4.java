1c6976454bc7d6cca675ae19f4237018557e64ad
表象 <156735229@qq.com>
2018-07-05 18:31:25 +0800

    up fix

--- course-web-server/src/main/java/com/huatu/tiku/course/netschool/api/v4/AppServiceV4.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/netschool/api/v4/AppServiceV4.java
@@ -1,6 +1,7 @@
 package com.huatu.tiku.course.netschool.api.v4;
 
 import com.huatu.tiku.course.bean.NetSchoolResponse;
+import com.huatu.tiku.course.netschool.api.fall.AppServiceV4Fallback;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,7 @@ import java.util.HashMap;
 /**
  * Created by lijun on 2018/5/18
  */
-@FeignClient(value = "o-course-service", path = "/lumenapi/v4/app", fallback = AppServiceV4.class)
+@FeignClient(value = "o-course-service", path = "/lumenapi/v4/app", fallback = AppServiceV4Fallback.class)
 public interface AppServiceV4 {
 
     @GetMapping("/lession/evaluate")
