2ec0e7129a812961fab2fccb99f5c1133f304be0
表象 <156735229@qq.com>
2018-07-27 15:49:15 +0800

    修改：新增弹幕数据字段

--- course-web-server/src/main/java/com/huatu/tiku/course/web/controller/v5/BarrageControllerV5.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/web/controller/v5/BarrageControllerV5.java
@@ -5,11 +5,14 @@ import com.huatu.common.utils.collection.HashMapBuilder;
 import com.huatu.springboot.web.version.mapping.annotation.ApiVersion;
 import com.huatu.tiku.common.bean.user.UserSession;
 import com.huatu.tiku.course.netschool.api.v5.BarrageServiceV5;
+import com.huatu.tiku.course.spring.conf.aspect.mapParam.LocalMapParam;
+import com.huatu.tiku.course.spring.conf.aspect.mapParam.LocalMapParamHandler;
 import com.huatu.tiku.course.util.ResponseUtil;
 import com.huatu.tiku.springboot.users.support.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -43,23 +46,11 @@ public class BarrageControllerV5 {
 
     /**
      * 弹幕提交
-     * @param userSession
-     * @param terminal
-     * @param classId
-     * @param content
-     * @param lessonId
-     * @return
-     * @throws BizException
      */
+    @LocalMapParam(checkToken = true)
     @PostMapping
-    public Object addBarrage(@Token UserSession userSession, @RequestHeader int terminal, long classId, String content, long lessonId) throws BizException{
-        Map<String,Object> params = HashMapBuilder.<String,Object>newBuilder()
-                .put("classId",classId)
-                .put("content",content)
-                .put("userName",userSession.getUname())
-                .put("lessonId",lessonId)
-                .put("terminal",terminal).build();
-
-        return ResponseUtil.build(barrageServiceV5.barrageAdd(params));
+    public Object addBarrage(long classId, String content, long lessonId, String videoNode, int background){
+        HashMap<String, Object> map = LocalMapParamHandler.get();
+        return ResponseUtil.build(barrageServiceV5.barrageAdd(map));
     }
 }
