6ace0c871198bc629bbdea39ca448f65d30b4176
表象 <156735229@qq.com>
2018-08-06 10:25:14 +0800

    弹幕提交接口修改

--- course-web-server/src/main/java/com/huatu/tiku/course/web/controller/v5/BarrageControllerV5.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/web/controller/v5/BarrageControllerV5.java
@@ -1,19 +1,15 @@
 package com.huatu.tiku.course.web.controller.v5;
 
 import com.huatu.common.exception.BizException;
-import com.huatu.common.utils.collection.HashMapBuilder;
 import com.huatu.springboot.web.version.mapping.annotation.ApiVersion;
-import com.huatu.tiku.common.bean.user.UserSession;
 import com.huatu.tiku.course.netschool.api.v5.BarrageServiceV5;
 import com.huatu.tiku.course.spring.conf.aspect.mapParam.LocalMapParam;
 import com.huatu.tiku.course.spring.conf.aspect.mapParam.LocalMapParamHandler;
 import com.huatu.tiku.course.util.ResponseUtil;
-import com.huatu.tiku.springboot.users.support.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author zhouwei
@@ -30,18 +26,15 @@ public class BarrageControllerV5 {
     /**
      * 弹幕获取
      */
+    @LocalMapParam(checkToken = true)
     @GetMapping
-    public Object getBarrages(@Token UserSession userSession, long classId, long lessonId,
+    public Object getBarrages(long classId,
+                              long lessonId,
+                              long videoNode,
                               @RequestParam(defaultValue = "1") int page,
-                              @RequestParam(defaultValue = "10") int pageSize) throws BizException{
-        Map<String,Object> params = HashMapBuilder.<String,Object>newBuilder()
-                .put("classId",classId)
-                .put("userName",userSession.getUname())
-                .put("page",page)
-                .put("pageSize",pageSize)
-                .put("lessonId",lessonId).build();
-
-        return ResponseUtil.build(barrageServiceV5.barrageList(params));
+                              @RequestParam(defaultValue = "10") int pageSize) throws BizException {
+        HashMap<String, Object> map = LocalMapParamHandler.get();
+        return ResponseUtil.build(barrageServiceV5.barrageList(map));
     }
 
     /**
@@ -49,7 +42,7 @@ public class BarrageControllerV5 {
      */
     @LocalMapParam(checkToken = true)
     @PostMapping
-    public Object addBarrage(long classId, String content, long lessonId, String videoNode, int background){
+    public Object addBarrage(long classId, String content, long lessonId, String videoNode, int background) {
         HashMap<String, Object> map = LocalMapParamHandler.get();
         return ResponseUtil.build(barrageServiceV5.barrageAdd(map));
     }
