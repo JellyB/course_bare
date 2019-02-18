0a12c9bf3ed626be2aabefd1c475d00589bc7aa0
jellyb <jelly_b@126.com>
2019-02-18 18:37:02 +0800

    update fallback response data

--- course-web-server/src/main/java/com/huatu/tiku/course/netschool/api/fall/CourseServiceV6FallBack.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/netschool/api/fall/CourseServiceV6FallBack.java
@@ -48,30 +48,34 @@ public class CourseServiceV6FallBack implements CourseServiceV6 {
     @Override
     public NetSchoolResponse calendarDetail(Map<String, Object> params) {
         log.warn("response from call back calendarDetail");
-        String key = CALENDAR_DETAIL_PRE + RequestUtil.getParamSign(params);
-        NetSchoolResponse response = FallbackCacheHolder.get(key);
-        if(response == null){
-            log.warn("obtain calendar detail not in fallbackHolder");
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            String defaultDate = simpleDateFormat.format(new Date());
-            DefaultCalenderDetail defaultCalenderDetail = DefaultCalenderDetail.builder()
-                    .msg(StringUtils.EMPTY)
-                    .type(0)
-                    .current_page("1")
-                    .data(Lists.newArrayList())
-                    .date(String.valueOf(params.getOrDefault("date", defaultDate)))
-                    .month(String.valueOf(params.getOrDefault("date", defaultDate)).split("-")[1])
-                    .day(String.valueOf(params.getOrDefault("date", defaultDate)).split("-")[2])
-                    .per_page(String.valueOf(params.getOrDefault("pageSize", 20)))
-                    .last_page(1)
-                    .total(0)
-                    .liveTotal(0)
-                    .from(1)
-                    .to(1)
-                    .build();
-            return NetSchoolResponse.newInstance(defaultCalenderDetail);
+        try{
+            String key = CALENDAR_DETAIL_PRE + RequestUtil.getParamSign(params);
+            NetSchoolResponse response = FallbackCacheHolder.get(key);
+            if(response == null){
+                log.warn("obtain calendar detail not in fallbackHolder");
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                String defaultDate = simpleDateFormat.format(new Date());
+                DefaultCalenderDetail defaultCalenderDetail = DefaultCalenderDetail.builder()
+                        .msg(StringUtils.EMPTY)
+                        .type(0)
+                        .current_page("1")
+                        .data(Lists.newArrayList())
+                        .date(String.valueOf(params.getOrDefault("date", defaultDate)))
+                        .month(String.valueOf(params.getOrDefault("date", defaultDate)).split("-")[1])
+                        .day(String.valueOf(params.getOrDefault("date", defaultDate)).split("-")[2])
+                        .per_page(String.valueOf(params.getOrDefault("pageSize", 20)))
+                        .last_page(1)
+                        .total(0)
+                        .liveTotal(0)
+                        .from(1)
+                        .to(1)
+                        .build();
+                return NetSchoolResponse.newInstance(defaultCalenderDetail);
+            }
+            return response;
+        }catch (Exception e){
+            return NetSchoolResponse.newInstance(DefaultCalenderDetail.builder().build());
         }
-        return response;
     }
 
     /**
