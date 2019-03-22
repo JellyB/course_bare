2ac0240a8cfd9db317fecc23e47dc00096c7a1d3
zhangchong <zhangchong01@huatu.com>
2019-03-22 16:20:07 +0800

    增加直播转回放回调接口

---
+++ course-web-server/src/main/java/com/huatu/tiku/course/dao/manual/CourseLiveBackLogMapper.java
@@ -0,0 +1,16 @@
+package com.huatu.tiku.course.dao.manual;
+
+import org.springframework.stereotype.Repository;
+
+import com.huatu.tiku.entity.CourseLiveBackLog;
+
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * 直播转回调mapper
+ * @author zhangchong
+ *
+ */
+@Repository
+public interface CourseLiveBackLogMapper extends Mapper<CourseLiveBackLog> {
+}
