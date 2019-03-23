b0d658af7d22d21b19063bc019a597d73de86b40
zhangchong <zhangchong01@huatu.com>
2019-03-24 00:24:21 +0800

    add test

--- course-web-server/src/test/java/com/huatu/tiku/course/test/PeriodTest.java
+++ course-web-server/src/test/java/com/huatu/tiku/course/test/PeriodTest.java
@@ -1,7 +1,6 @@
 package com.huatu.tiku.course.test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
