bd9593848181bb4cc5dbc296aafa9341f2e727fa
jellyb <jelly_b@126.com>
2019-03-26 17:10:13 +0800

    学习报告大纲展示版本控制

---
+++ course-web-server/src/main/java/com/huatu/tiku/course/service/v1/VersionControlService.java
@@ -0,0 +1,38 @@
+package com.huatu.tiku.course.service.v1;
+
+import com.huatu.tiku.common.AppVersionEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+/**
+ * 描述：
+ *
+ * @author biguodong
+ * Create time 2019-03-26 4:46 PM
+ **/
+
+@Service
+@Slf4j
+public class VersionControlService {
+    private static int IosVersion = 7110;
+    private static int AndroidVersion = 7111;
+
+
+    /**
+     * 版本检测是否展示学习报告
+     * @param terminal
+     * @param cv
+     * @return
+     */
+    public boolean checkLearnReportShow(int terminal, String cv){
+        int versionCount = Integer.valueOf(StringUtils.rightPad(cv.replaceAll("\\.", ""), 3, '0'));
+        if(terminal == AppVersionEnum.TerminalTypeEnum.IOS.getValue() && versionCount >= IosVersion){
+            return true;
+        }else if(terminal == AppVersionEnum.TerminalTypeEnum.ANDROID.getValue() && versionCount >= AndroidVersion){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
