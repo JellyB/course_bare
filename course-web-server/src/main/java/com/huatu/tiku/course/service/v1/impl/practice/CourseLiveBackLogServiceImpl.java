6a190df67560a139cfe339b63fa0241003e9be22
jellyb <jelly_b@126.com>
2019-03-28 13:38:31 +0800

    直播转回放数据补充方案

--- course-web-server/src/main/java/com/huatu/tiku/course/service/v1/impl/practice/CourseLiveBackLogServiceImpl.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/service/v1/impl/practice/CourseLiveBackLogServiceImpl.java
@@ -1,8 +1,16 @@
 package com.huatu.tiku.course.service.v1.impl.practice;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.google.common.collect.Maps;
+import com.huatu.tiku.course.bean.NetSchoolResponse;
+import com.huatu.tiku.course.netschool.api.v6.LessonServiceV6;
+import com.huatu.tiku.course.util.ResponseUtil;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.huatu.tiku.course.service.v1.practice.CourseLiveBackLogService;
@@ -23,6 +31,10 @@ import tk.mybatis.mapper.weekend.WeekendSqls;
 public class CourseLiveBackLogServiceImpl extends BaseServiceHelperImpl<CourseLiveBackLog>
 		implements CourseLiveBackLogService {
 
+	@Autowired
+	private LessonServiceV6 lessonService;
+
+
 	public CourseLiveBackLogServiceImpl() {
 		super(CourseLiveBackLog.class);
 	}
@@ -37,6 +49,27 @@ public class CourseLiveBackLogServiceImpl extends BaseServiceHelperImpl<CourseLi
 		List<CourseLiveBackLog> courseLiveBackLogList = selectByExample(example);
 		if (CollectionUtils.isNotEmpty(courseLiveBackLogList)) {
 			return courseLiveBackLogList.get(0);
+		}else{
+			log.info("调用php获取直播回放对应的直播课件id:直播回放id:{},房间id:{}", coursewareId, roomId);
+			Map<String,Object> params = Maps.newHashMap();
+			params.put("liveBackCoursewareId",coursewareId);
+			params.put("roomId", roomId);
+			NetSchoolResponse netSchoolResponse = lessonService.obtainLiveWareId(params);
+			if(ResponseUtil.isSuccess(netSchoolResponse)){
+				LinkedHashMap<String,Object> result = (LinkedHashMap<String,Object>) netSchoolResponse.getData();
+				long liveCourseWareId = MapUtils.getLong(result, "liveCoursewareId");
+				if(liveCourseWareId == 0){
+					return null;
+				}
+				CourseLiveBackLog courseLiveBackLog = new CourseLiveBackLog();
+				courseLiveBackLog.setLiveBackCoursewareId(coursewareId);
+				courseLiveBackLog.setRoomId(roomId);
+				courseLiveBackLog.setLiveCoursewareId(liveCourseWareId);
+				courseLiveBackLog.setLiveBackCoursewareId(coursewareId);
+				courseLiveBackLog.setCreatorId(10L);
+				insert(courseLiveBackLog);
+				return courseLiveBackLog;
+			}
 		}
 		return null;
 	}
