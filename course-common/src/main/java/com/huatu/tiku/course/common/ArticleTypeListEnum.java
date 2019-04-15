965ccb6f0dba6db20ee3378cba6f5b1f66ed34fd
lizhenjuan <1920319148>
2019-04-15 14:50:23 +0800

    add tab类别

--- course-common/src/main/java/com/huatu/tiku/course/common/ArticleTypeListEnum.java
+++ course-common/src/main/java/com/huatu/tiku/course/common/ArticleTypeListEnum.java
@@ -12,11 +12,11 @@ import lombok.Getter;
 @Getter
 public enum ArticleTypeListEnum {
 
-
     HOT(1, 1, "热门"),
     SHI_ZHENG_HOT(2, 2, "时政热点"),
     ANSWER_SKILL(3, 3, "解题技巧"),
-    BKZD(4, 4, "备考指导");
+    BKZD(4, 4, "备考指导"),
+    KQSD(5, 5, "考情速递");
 
     private Integer code;
     private Integer sort;
