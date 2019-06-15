eba5bf597980bc4443f252f8b5f738bae85f90b1
zhangchong <zhangchong01@huatu.com>
2019-06-15 16:08:53 +0800

    modify report logic

--- course-web-server/src/main/java/com/huatu/tiku/course/mq/listeners/ErrorLogReportListener.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/mq/listeners/ErrorLogReportListener.java
@@ -1,13 +1,16 @@
 package com.huatu.tiku.course.mq.listeners;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.alibaba.fastjson.JSONObject;
 import com.dingtalk.chatbot.DingtalkChatbotClient;
 import com.dingtalk.chatbot.message.TextMessage;
-import com.huatu.tiku.course.consts.RabbitMqConstants;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.huatu.tiku.common.bean.report.ExceptionReportMessage;
+import com.rabbitmq.client.Channel;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,30 +22,31 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 @Component
-public class ErrorLogReportListener {
-	
+public class ErrorLogReportListener implements ChannelAwareMessageListener {
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Value("${dingding.webhook:https://oapi.dingtalk.com/robot/send?access_token=338405433f6b9a54abf4d1e1681857c005a304a3ddbe19b38ae4ea869bd2adbd}")
 	private String webhook;
-	
-	@RabbitListener(queues=RabbitMqConstants.DATA_REPORT)
-	public void onMessage(String message) {
-		log.error("error log:{}", message);
-		JSONObject json = JSONObject.parseObject(message);
-		if(json != null) {
-			String stacktrace = json.getString("stacktrace");
-			sendMsg(stacktrace);
+
+	@Override
+	public void onMessage(Message message, Channel channel) throws Exception {
+		ExceptionReportMessage msg = objectMapper.readValue(message.getBody(), ExceptionReportMessage.class);
+		if (msg != null) {
+			sendMsg(msg.getStacktrace());
 		}
-		
+
 	}
-	
-	private void sendMsg(String content){
+
+	private void sendMsg(String content) {
 		try {
 			DingtalkChatbotClient client = new DingtalkChatbotClient();
 			TextMessage msg = new TextMessage(content);
 			msg.setIsAtAll(true);
 			client.send(webhook, msg);
 		} catch (Exception e) {
-			log.error("dingding sendMsg error:{}",e);
+			log.error("dingding sendMsg error:{}", e);
 		}
 	}
 
