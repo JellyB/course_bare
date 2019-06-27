ca3f2ed3e753ea466a9d70e65dc3c0e2e438b6df
zhangchong <zhangchong01@huatu.com>
2019-06-27 20:33:29 +0800

    format report log

--- course-web-server/src/main/java/com/huatu/tiku/course/mq/listeners/ErrorLogReportListener.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/mq/listeners/ErrorLogReportListener.java
@@ -34,7 +34,9 @@ public class ErrorLogReportListener implements ChannelAwareMessageListener {
 	public void onMessage(Message message, Channel channel) throws Exception {
 		ExceptionReportMessage msg = objectMapper.readValue(message.getBody(), ExceptionReportMessage.class);
 		if (msg != null) {
-			sendMsg("主机:"+msg.getHost()+"\r\n"+msg.getStacktrace());
+
+			sendMsg("主机:" + msg.getHost() + "\r\n" + "应用名称:" + msg.getApplication() + "\r\n" + "方法名:" + msg.getMethod()
+					+ "\r\n" + msg.getStacktrace());
 		}
 
 	}
