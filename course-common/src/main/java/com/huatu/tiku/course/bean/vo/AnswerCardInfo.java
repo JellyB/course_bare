bdf10bb26f42dcb429422a0525cda285f81241bc
jellyb <jelly_b@126.com>
2019-08-29 17:35:38 +0800

    update

---
+++ course-common/src/main/java/com/huatu/tiku/course/bean/vo/AnswerCardInfo.java
@@ -0,0 +1,41 @@
+package com.huatu.tiku.course.bean.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 描述：
+ *
+ * @author biguodong
+ * Create time 2019-08-29 5:28 PM
+ **/
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class AnswerCardInfo {
+
+    /**
+     * 1 行测 2 申论
+     */
+    private int type;
+    private int status;
+    private int wcount;
+    private int ucount;
+    private int rcount;
+    private int qcount;
+    private long  id;
+
+    @Builder
+    public AnswerCardInfo(int type, int status, int wcount, int ucount, int rcount, int qcount, long id) {
+        this.type = type;
+        this.status = status;
+        this.wcount = wcount;
+        this.ucount = ucount;
+        this.rcount = rcount;
+        this.qcount = qcount;
+        this.id = id;
+    }
+}
