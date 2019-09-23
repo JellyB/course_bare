b0ab43bbcc82ebdfc6054fc178becf5b5bab72af
lizhenjuan <1920319148>
2019-09-23 15:31:08 +0800

    level 游客模式

--- course-web-server/src/main/java/com/huatu/tiku/course/web/controller/v3/UserLevelControllerV3.java
+++ course-web-server/src/main/java/com/huatu/tiku/course/web/controller/v3/UserLevelControllerV3.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -27,28 +28,34 @@ public class UserLevelControllerV3 {
 
 
     /**
-     *
      * @param userSession
      * @return
      */
     @RequestMapping("/reward/view")
-    public Object getUserRewardProcess(@Token UserSession userSession){
+    public Object getUserRewardProcess(@Token UserSession userSession) {
         return rewardBizService.getRewardView(userSession.getId());
     }
 
     /**
      * 获取用户等级信息
+     * update 支持游客模式
+     *
      * @param userSession
      * @return
      */
     @RequestMapping("/level")
-    public Object getUserLevel(@Token UserSession userSession){
-        Map<String,Object> params = HashMapBuilder.newBuilder()
-                .put("username",userSession.getUname())
-                .put("action",1)
-                .buildUnsafe();
-        return ResponseUtil.build(userLevelServiceV3.getUserLevel(RequestUtil.encryptParams(params)),true);
+    public Object getUserLevel(@Token(check = false) UserSession userSession) {
+        if (null != userSession) {
+            Map<String, Object> params = HashMapBuilder.newBuilder()
+                    .put("username", userSession.getUname())
+                    .put("action", 1)
+                    .buildUnsafe();
+            return ResponseUtil.build(userLevelServiceV3.getUserLevel(RequestUtil.encryptParams(params)), true);
+        } else {
+            HashMap map = new HashMap();
+            map.put("level", 0);
+            return map;
+        }
     }
-
 
 }
