dbeee7df95fd039fb9254609976383305c0703fb
biguodong <biguodong@jd.com>
2020-02-29 20:10:54 +0800

    test 中文乱码问题

---
+++ course-web-server/src/main/java/com/huatu/tiku/course/web/controller/v8/TestController.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2014-2025 JD.COM All Right Reserved.
+ *
+ * @file£º TestController.java   project: course-server
+ * @creator: biguodong
+ * @date: 2020/2/29
+ */
+
+package com.huatu.tiku.course.web.controller.v8;
+
+public class TestController {
+}
